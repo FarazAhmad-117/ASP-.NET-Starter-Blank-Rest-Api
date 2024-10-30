e27c1569a72b75d9e4a6dab17a0e66b07bd08324
FarazAhmad-117 <farazahmad31048@gmail.com>
2024-10-30 13:22:07 -0700

    init

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Area to configure all the services
+
+
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/api", () => "Hello Api!");
+
+// For some json data
+
+app.MapGet("/data", () => new
+{
+    success = true,
+    message = "Hello Boi",
+    Time = DateTime.Now
+});
+
+
+app.Run();
